32fd62a4e29c083f8a97b397a263ab66a23d9ac3
drevitsky <4227723@gmail.com>
2019-05-14 00:18:45 +0300

    first commit

---
+++ src/js/bootstrap.js
@@ -0,0 +1,16 @@
+//= ./util.js
+/// = ./alert.js
+//= ./button.js
+//= ./carousel.js
+//= ./collapse.js
+//= ./dropdown.js
+/// = ./tab.js
+/// = ./scrollspy.js
+//= ./tooltip.js
+//= ./popover.js
+
+//= ./modal.js
+
+//= ./isotope.js
+//= ./smooth-scroll.js
+//= ./scripts.js
